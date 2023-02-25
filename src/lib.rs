404256dddfb61215d9cdfdb817e7f2cc8e6d5bac
siIverfish <ivan.g.zimmerman@gmail.com>
2023-02-25 14:08:40 -0800

    start work on encoding

--- src/lib.rs
+++ src/lib.rs
@@ -50,13 +50,13 @@ pub mod bytes_manipulation {
         }
     }
     
-    pub fn multiply_bytes(larger: Vec<u8>, smaller: Vec<u8>) -> Vec<u8> { // TODO failing: test_max_multiply_bytes
+    pub fn multiply_bytes(larger: Vec<u8>, smaller: Vec<u8>) -> Vec<u8> {
         let mut new_bytes: Vec<u8> = vec![0; larger.len() * 2];
     
         for (index_1, byte_1) in (&larger).iter().rev().enumerate() {
             for (index_2, byte_2) in (&smaller).iter().rev().enumerate()  {
                 let mut mult_result_vector: Vec<u8> = vec![0; index_1 + index_2 + 2];
-                
+
                 (mult_result_vector[0], mult_result_vector[1]) = split_word((*byte_1 as u16) * (*byte_2 as u16));
 
                 if mult_result_vector[0] == 0 {
@@ -73,5 +73,23 @@ pub mod bytes_manipulation {
         }
 
         new_bytes[num_leading_zeroes..].to_vec()
+    }
+}
+
+pub mod storage {
+    use std::collections::HashMap;
+
+    pub fn encode(byte_alphabet: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
+        // create mapping from alphabet to data number
+        let mut map: HashMap<u8, u8> = HashMap::new();
+        for (index, item) in byte_alphabet.into_iter().enumerate() {
+            map.insert(item, index as u8);
+        }
+
+        todo!();
+    }
+
+    pub fn decode(alphabet: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
+        todo!();
     }
 }
