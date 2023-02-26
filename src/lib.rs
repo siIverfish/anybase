d746556db067906b1cc7bcca1864755b8cdd6aad
siIverfish <ivan.g.zimmerman@gmail.com>
2023-02-26 14:00:22 -0800

    almost works?

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,17 @@ pub mod storage {
     use num_bigint::BigUint; 
 
     pub fn encode(byte_alphabet: &Vec<u8>, data: &Vec<u8>) -> Vec<u8> {
+        println!("ENCODING ALPHABET LEN: {}", byte_alphabet.len());
+
+        // todo verify alphabet
+
         // create mapping from alphabet to data number
         let mut alphabet_map: HashMap<u8, u8> = HashMap::new();
         for (index, &item) in byte_alphabet.into_iter().enumerate() {
             alphabet_map.insert(item, (index as u8) + 1);
         }
+
+        println!("ENCODING ALPHABET MAP: {:?}", alphabet_map);
 
         let mut new_data: BigUint = BigUint::new( vec![0] );
         let big_alphabet_len = BigUint::from( byte_alphabet.len() );
@@ -18,7 +24,6 @@ pub mod storage {
         for (i, datum) in data
             .into_iter()
             .map( |x| alphabet_map[x] )
-            .rev() // byte order? possibly change
             .enumerate() 
         {
             let datum: BigUint = BigUint::from(datum) * big_alphabet_len.pow(i as u32);
@@ -35,21 +40,34 @@ pub mod storage {
             alphabet_map.insert((index as u8) + 1, item);
         }
         
+        println!("ALPHABET MAP: {:?}", alphabet_map);
+
         let alphabet_len_big: BigUint = BigUint::from(byte_alphabet.len() as u32);
 
         // get num of chars in data
         let mut approx_data_len: usize = ((data.len() as f32) * 256.0_f32.log(byte_alphabet.len() as f32)).ceil() as usize;
         
+        println!("DECODED DATA LEN (FIRST ESTIMATE): {}", approx_data_len);
+
         let mut data: BigUint = BigUint::from_bytes_be(data);
         let mut new_data: Vec<u8> = Vec::new();
 
         while alphabet_len_big.pow(approx_data_len as u32) > data {
+            println!("Too big! {}  vs {}", alphabet_len_big.pow(approx_data_len as u32), data);
             approx_data_len -= 1_usize;
         }
         // data len SHOULD be correct now
+        println!("DECODED DATA LEN: {}", approx_data_len);
+
         let alphabet_len: usize = byte_alphabet.len();
-        for _i in approx_data_len..1 {
-            new_data.push( alphabet_map[ &((&data % alphabet_len).to_bytes_le()[0] & 0xFF) ] ); // even more haunted
+        for i in 0..approx_data_len {
+            println!("DATA: {} {}", i, data);
+            let key: &u8 = &(((&data % alphabet_len).to_bytes_le()[0] & 0xFF));
+            println!("before [0] &: {:?}", ( &data % alphabet_len ).to_bytes_le() );
+            println!("KEY: {}", key);
+            println!("ADDING {}", alphabet_map[ key ]);
+            println!();
+            new_data.push( alphabet_map[ key ] );
             data /= alphabet_len;
         }
 
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,2 +1,23 @@
 
+
+
 // todo later
+#[cfg(test)]
+mod encode_decode_test {
+    use anybase::storage::*;
+
+    macro_rules! encode_and_decode {
+        ( $name:ident, $alphabet:expr, $data:expr ) => {
+            #[test]
+            pub fn $name() {
+                assert_eq!($data, decode(&$alphabet, &encode(&$alphabet, &$data)));
+            }
+        };
+    }
+
+    encode_and_decode!(zero_at_end, vec![0, 1], vec![1, 0]);
+    encode_and_decode!(same_as_alphabet, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9], vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    encode_and_decode!(two_letter_alphabet, vec![0, 1], vec![0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0]);
+
+    encode_and_decode!(random_y, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9], vec![3, 2, 5, 7, 1, 3, 6, 8, 5, 4, 6, 8, 0, 4, 2, 1, 2, 5, 4]);
+}
